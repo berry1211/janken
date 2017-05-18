f68dc8b905a2d9bb180c346b1a11fc4798d28dc7
berry1211 <nnhysd@gmail.com>
2017-05-18 11:43:31 +0900

    相手に合わせて手を変える

--- docs/player.js
+++ docs/player.js
@@ -2,15 +2,53 @@ const GU = 0;
 const CHOKI = 1;
 const PA = 2;
 
-
 let matches = 0;
-function action(opponent){
+
+function countup(){
   matches = matches + 1;
-  const remainder = matches % 3;
-  const isEven = remainder == 0;
-  console.log(`${matches} % 3 = ${remainder} : ${isEven}`);
-  if(isEven){
-    return CHOKI
-  }
+}
+
+// チョキ大好きマンに対するアクション
+function actionAgainstChokiLover(){
   return GU;
+}
+
+// 表か裏かマンに対するアクション
+function actionAgainstOmoteUra(){
+  const isEven = matches % 2 == 0;
+  let result = PA;
+  if(isEven){
+    result = GU;
+  }
+  return result;
+}
+
+// 帰ってきた表か裏かマンに対するアクション
+function actionAgainstOmoteUra2(){
+  const isEven = matches % 3 == 0;
+  let result = GU;
+  if(isEven){
+    result = CHOKI;
+  }
+  return result;
+}
+
+/*
+チョキ大好きマン：fighter::choki-lover
+表か裏かマン：fighte::odd-even
+帰ってきた表か裏かマン：fighter::on-third
+*/
+
+function action(oppornent){
+  console.log(oppornent);
+  let result = GU;
+  countup();
+  if(oppornent == "fighter::choki-lover"){
+    result = actionAgainstChokiLover();
+  } else if(oppornent == "fighter::odd-even"){
+    result = actionAgainstOmoteUra();
+  } else {
+    result = actionAgainstOmoteUra2();
+  }
+  return result;
 }
