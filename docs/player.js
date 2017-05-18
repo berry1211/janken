47f1bb8e1c8160edc3228365425dc5c1783a386b
berry1211 <nnhysd@gmail.com>
2017-05-18 12:10:17 +0900

    順番に出していくマンに勝つ

--- docs/player.js
+++ docs/player.js
@@ -3,13 +3,69 @@ const CHOKI = 1;
 const PA = 2;
 
 let matches = 0;
-function action(oppornent){
+
+function countup(){
   matches = matches + 1;
-  const remainder = matches % 2;
-  const isEven = remainder == 0;
-  console.log(`${matches} % 2 = ${remainder} : ${isEven}`);
+}
+
+// チョキ大好きマンに対するアクション
+function actionAgainstChokiLover(){
+  return GU;
+}
+
+// 表か裏かマンに対するアクション
+function actionAgainstOmoteUra(){
+  const isEven = matches % 2 == 0;
+  let result = PA;
   if(isEven){
-    return GU
+    result = GU;
   }
-  return PA;
+  return result;
+}
+
+// 帰ってきた表か裏かマンに対するアクション
+function actionAgainstOmoteUra2(){
+  const isEven = matches % 3 == 0;
+  let result = GU;
+  if(isEven){
+    result = CHOKI;
+  }
+  return result;
+}
+
+// 順番に出していくマンに対するアクション
+function actionAgainstJunban(){
+  const isEven = matches % 3 == 0;
+  let a = matches % 3 == 1;
+  if(isEven){
+    result = CHOKI;
+  } else if(a){
+    result = PA;
+  } else{
+    result = GU;
+  }
+  return result;
+}
+
+/*
+チョキ大好きマン：fighter::choki-lover
+表か裏かマン：fighte::odd-even
+帰ってきた表か裏かマン：fighter::on-third
+順番に出していくマン：fighter::rotation
+*/
+
+function action(oppornent){
+  console.log(oppornent);
+  let result = GU;
+  countup();
+  if(oppornent == "fighter::choki-lover"){
+    result = actionAgainstChokiLover();
+  } else if(oppornent == "fighter::odd-even"){
+    result = actionAgainstOmoteUra();
+  } else if(oppornent == "fighter::on-third"){
+    result = actionAgainstOmoteUra2();
+  } else {
+    result = actionAgainstJunban();
+  }
+  return result;
 }
